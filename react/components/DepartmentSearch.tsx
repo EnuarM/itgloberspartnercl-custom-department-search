e8a178fde629f5d6310424a50b555c4ce97cc646
EnuarM <99690878+EnuarM@users.noreply.github.com>
2022-12-15 18:45:55 -0500

    Component done

---
+++ react/components/DepartmentSearch.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useQuery } from 'react-apollo'
+import QUERY_VALUE from '../graphql/getDepartmentGroup.graphql'
+import DepartmentGroup from './DepartmentGroup'
+import { SearchBar } from 'vtex.store-components'
+
+const DepartmentSearch = () => {
+
+  const { data, loading } = useQuery(QUERY_VALUE)
+  const [slug, setSlug]= useState("")
+
+  console.log("Mis slug seleccionado es: ", slug)
+
+  return (
+    loading
+    ?
+    <div>Loading...</div>
+    :
+    <div className='flex'>
+      <DepartmentGroup
+        departments={data?.categories[0]?.children}
+        handleSetSlug={setSlug}
+        />
+
+      <SearchBar
+        customSearchPageUrl={slug}
+        placeholder="Que buscas en VTEX"
+        displayMode="search-and-clear-buttons"
+      />
+    </div>
+  )
+}
+
+export default DepartmentSearch
